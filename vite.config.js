a4b69b9ec807f1d1fae00105e10804aa7466c0e7
Asdeire <kaoleksandr7@gmail.com>
2024-11-17 15:51:21 +0200

    Minimization of files and images is configured

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,38 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import viteImagemin from 'vite-plugin-imagemin'; 
 
-// https://vite.dev/config/
 export default defineConfig({
-  root: 'src', 
-  plugins: [react(), svgr()]  
+  root: 'src',
+
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true, 
+      },
+    },
+    cssCodeSplit: true, 
+
+    rollupOptions: {
+      plugins: [
+        viteImagemin({
+          mozjpeg: {
+            quality: 75, 
+          },
+          pngquant: {
+            quality: [0.6, 0.8], 
+          },
+          svgo: {
+            plugins: [
+              { removeViewBox: false }, 
+            ],
+          },
+        }),
+      ],
+    },
+  },
+
+  plugins: [react(), svgr()] 
 });
